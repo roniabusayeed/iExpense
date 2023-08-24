f729b665aee0712ff61d4b0915879e11f52fc902
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-23 23:14:54 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Abu Sayeed Roni on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
